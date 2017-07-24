ac10ffd11247f0d268554e233d6ef48fdc08adc8
Catalin @ LM Machine <catalin@localhost>
2017-07-24 14:49:59 +0300

    -

--- webh-nl.py
+++ webh-nl.py
@@ -12,6 +12,3 @@ print 'Copying template vhosts config files and make necessary replacements ...'
 for file in glob.glob("files/vhosts/fqdn-*.conf"):
 	copyfile(file, webh_config_path+'/'+site_name+file.split('fqdn')[1])
 	os.popen("sed -i 's/{SITE_NAME}/"+site_name+"/g' "+webh_config_path+'/'+site_name+file.split('fqdn')[1])
-
-	
-	
