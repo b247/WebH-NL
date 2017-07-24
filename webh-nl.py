72077463f777223e181a67cfc3000d25b8100658
Catalin @ LM Machine <catalin@localhost>
2017-07-24 13:19:17 +0300

    -

--- webh-nl.py
+++ webh-nl.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
 import glob, os
+from shutil import copyfile
 
 
 webh_config_path = '/home/catalin/Desktop/etc/www'
 site_name = 'fqdn.tld'
 
-print os.popen("echo $(pwd)").read()
-exit(0)
 
-os.chdir(webh_config_path)
-print ''
-for file in glob.glob("fqdn-*.conf"):
-	print "Configuring "+file+" ..."
-	print os.popen("sed -i 's/{SITE_NAME}/"+site_name+"/g' "+file).read()
+print 'Copying template vhosts config files and make necessary replacements ...'
+for file in glob.glob("files/vhosts/fqdn-*.conf"):
+	copyfile(file, webh_config_path+'/'+site_name+file.split('fqdn')[1])
+	os.popen("sed -i 's/{SITE_NAME}/"+site_name+"/g' "+webh_config_path+'/'+site_name+file.split('fqdn')[1])
+
 	
 	
