2615bc2bf65f44f91cabbd3fb533090887f590d2
Catalin @ LM Machine <catalin@localhost>
2017-07-24 12:44:05 +0300

    -

--- helpers/webh-nl.py
+++ helpers/webh-nl.py
@@ -1,1 +1,15 @@
-1
+#!/usr/bin/env python
+
+import glob, os
+
+
+webh_config_path = '/home/catalin/Desktop/etc/www'
+site_name = 'fqdn.tld'
+
+os.chdir(webh_config_path)
+print ''
+for file in glob.glob("fqdn-*.conf"):
+	print "Configuring "+file+" ..."
+	print os.popen("sed -i 's/{SITE_NAME}/"+site_name+"/g' "+file).read()
+	
+	
