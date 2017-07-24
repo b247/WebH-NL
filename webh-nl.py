9b1111017ef2c73047228651676b1aa505d8e488
Catalin @ LM Machine <catalin@localhost>
2017-07-24 17:41:48 +0300

    Mon Jul 24 17:41:48 EEST 2017

--- webh-nl.py
+++ webh-nl.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
-# include this part into final project
-import glob, os
-from shutil import copyfile
 
+
+import glob, os
+
+# include this part into final project
+from shutil import copyfile
 
 webh_config_path = '/home/catalin/Desktop/etc/www'
 site_name = 'fqdn.tld'
 
-
+# include this part into final project
 print 'Copying template vhosts config files and make necessary replacements ...'
 for file in glob.glob("files/vhosts/fqdn-*.conf"):
 	copyfile(file, webh_config_path+'/'+site_name+file.split('fqdn')[1])
 	os.popen("sed -i 's/{SITE_NAME}/"+site_name+"/g' "+webh_config_path+'/'+site_name+file.split('fqdn')[1])
+# include this part into final project
